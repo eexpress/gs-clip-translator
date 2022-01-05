61d7ba2a5cde4f9a5d6d09c0cc7bd3e5d7adfb6d
eexpress <eexpress@163.com>
2022-01-05 11:52:49 +0800

    PRIMARY获取

--- extension.js
+++ extension.js
@@ -14,24 +14,24 @@ const Main = imports.ui.main;
 const PanelMenu = imports.ui.panelMenu;
 const PopupMenu = imports.ui.popupMenu;
 
-	let action = 0;	// 0 is none, 1 is from, 2 is to.
+	let action = 0;	// 0 is none, 1 is primary_icon, 2 is secondary_icon.
 	let from = 'auto';
 	let to = 'zh';
 
 const Indicator = GObject.registerClass(
 class Indicator extends PanelMenu.Button {
-    _init() {
-        super._init(0.0, _('Clip Translator'));
+	_init() {
+		super._init(0.0, _('Clip Translator'));
 
-        this.add_child(new St.Icon({
-            //~ icon_name: 'face-devilish-symbolic',
-            gicon: local_gicon("trans-symbolic"),
-            icon_size: 30,
-            style_class: 'system-status-icon',
-        }));
-        //~ ----------------------------------------
-        //~ ----------------------------------------
-        const minput = new PopupMenu.PopupBaseMenuItem({reactive: false});
+		this.add_child(new St.Icon({
+			//~ icon_name: 'face-devilish-symbolic',
+			gicon: local_gicon("trans-symbolic"),
+			icon_size: 30,
+			style_class: 'system-status-icon',
+		}));
+		//~ ----------------------------------------
+		//~ ----------------------------------------
+		const minput = new PopupMenu.PopupBaseMenuItem({reactive: false});
 		const input = new St.Entry({
 			name: 'searchEntry',
 			style_class: 'big_text',
@@ -47,10 +47,10 @@ class Indicator extends PanelMenu.Button {
 		//~ input.clutter_text.connect('activate', (actor) => {});
 		minput.add(input);
 		this.menu.addMenuItem(minput);
-        //~ ----------------------------------------
-        const mflag = new PopupMenu.PopupBaseMenuItem({reactive: false});
-        //~ const mflag = new PopupMenu.PopupMenuItem('');
-        ['globa-symbolic','ar','de','en','es','fr','ja','ko','ru','zh'].forEach(showicon);
+		//~ ----------------------------------------
+		const mflag = new PopupMenu.PopupBaseMenuItem({reactive: false});
+		//~ const mflag = new PopupMenu.PopupMenuItem('');
+		['globa-symbolic','ar','de','en','es','fr','ja','ko','ru','zh'].forEach(showicon);
 		function showicon(str){
 			let icon = new St.Icon({ gicon: local_gicon(str)});
 			let butt = new St.Button({ can_focus: true, child: icon });
@@ -60,13 +60,21 @@ class Indicator extends PanelMenu.Button {
 		mflag.visible = false;
 
 		this.menu.addMenuItem(mflag);
-        //~ ----------------------------------------
+		//~ ----------------------------------------
+		this._selection = global.display.get_selection();
+		this._clipboard = St.Clipboard.get_default();
+		this._ownerChangedId = this._selection.connect('owner-changed', () => {
+			this._clipboard.get_text(St.ClipboardType.PRIMARY, (clipboard, text) => {
+				input.text = text;
+			});
+		});
+		//~ ----------------------------------------
 		function local_gicon(str){
 			return Gio.icon_new_for_string(
 			ExtensionUtils.getCurrentExtension().path+"/img/"+str+".svg");
 		}
-        //~ ----------------------------------------
-        function choose_lang(str){
+		//~ ----------------------------------------
+		function choose_lang(str){
 			switch(action) {
 				 case 1:
 					input.set_primary_icon(new St.Icon({gicon: local_gicon(str)}));
@@ -83,29 +91,33 @@ class Indicator extends PanelMenu.Button {
 			action = 0;
 			log(`${from} -> ${to}`);
 		}
-        //~ ----------------------------------------
-        //~ ----------------------------------------
-    }
+		//~ ----------------------------------------
+		//~ ----------------------------------------
+	}
+
+	destroy() {
+		this._selection.disconnect(this._ownerChangedId);
+	}
 });
 
 class Extension {
-    constructor(uuid) {
-        this._uuid = uuid;
+	constructor(uuid) {
+		this._uuid = uuid;
 
-        ExtensionUtils.initTranslations(GETTEXT_DOMAIN);
-    }
+		ExtensionUtils.initTranslations(GETTEXT_DOMAIN);
+	}
 
-    enable() {
-        this._indicator = new Indicator();
-        Main.panel.addToStatusArea(this._uuid, this._indicator);
-    }
+	enable() {
+		this._indicator = new Indicator();
+		Main.panel.addToStatusArea(this._uuid, this._indicator);
+	}
 
-    disable() {
-        this._indicator.destroy();
-        this._indicator = null;
-    }
+	disable() {
+		this._indicator.destroy();
+		this._indicator = null;
+	}
 }
 
 function init(meta) {
-    return new Extension(meta.uuid);
+	return new Extension(meta.uuid);
 }
