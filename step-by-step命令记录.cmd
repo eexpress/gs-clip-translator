3606dd7b6a327ceffab4cf35c2cef50e955aa242
eexpress <eexpress@163.com>
2022-02-20 13:44:17 +0800

    step.cmd

--- step-by-step命令记录.cmd
+++ step-by-step命令记录.cmd
@@ -4,11 +4,20 @@
 2) 指示器   –  添加图标到顶栏
 模板 [1-2]: 2
 
+# 模板，可用补全
+⭕ gnome-extensions create --template=
+indicator  plain
+
 ⭕ geany metadata.json extension.js
 
-⭕ xgettext --from-code=UTF-8 -o sample.pot *.js
+⭕ xgettext --from-code=UTF-8 -o _sample.pot *.js
+⭕ mkdir po
 
-⭕ msginit -l zh_CN.UTF8 -i sample.pot -o sample.po
+# 如果不想被问邮箱，以及下载翻译组模板，可用带 --no-translator 参数。
+⭕ msginit --no-translator -l zh_CN.UTF8 -i _sample.pot -o _sample.po
+已创建 _sample.po。
+
+⭕ msginit -l zh_CN.UTF8 -i _sample.pot -o po/zh_CN.po
 新的翻译库应该包含您的电子邮件地址，这样用户就可以对您的翻译提出意见，
 同时软件维护者也可以在出现技术问题时联络您。
 
@@ -25,47 +34,25 @@ Please visit your translation team's homepage at
 and consider joining your translation team's mailing list
   <i18n-zh@googlegroups.com>
 
-已创建 sample.po。
+已创建 po/zh_CN.po。
 
-⭕ geany sample.po
+# 询问邮箱的差异。
+⭕ diff _sample-1.po _sample.po
+5c5
+< # eexpss <eexpss@gmail.com>, 2022.
+---
+> # Automatically generated, 2022.
+13,14c13,14
+< "Last-Translator: eexpss <eexpss@gmail.com>\n"
+< "Language-Team: Chinese (simplified) <i18n-zh@googlegroups.com>\n"
+---
+> "Last-Translator: Automatically generated\n"
+> "Language-Team: none\n"
 
-⭕ mkdir -p locale/zh_CN/LC_MESSAGES/
+⭕ geany po/zh_CN.po
 
-⭕ msgfmt -c -v sample.po -o locale/zh_CN/LC_MESSAGES/clip-translator.mo
-sample.po:8: 警告： 文件头“Project-Id-Version”仍然是默认值未改变
-3 条已翻译消息.
-
-⭕ zip -r clip-translator@eexpss.gmail.com.zip extension.js metadata.json stylesheet.css md5.js img/ locale/
-
-⭕ unzip -l clip-translator@eexpss.gmail.com.zip
-Archive:  clip-translator@eexpss.gmail.com.zip
-  Length      Date    Time    Name
----------  ---------- -----   ----
-     5779  2022-01-07 11:46   extension.js
-      305  2022-01-06 13:24   metadata.json
-      138  2022-01-05 15:07   stylesheet.css
-     7498  2015-11-24 19:07   md5.js
-        0  2022-01-06 16:20   img/
-    16008  2022-01-04 15:30   img/global-symbolic.svg
-     1644  2022-01-04 00:44   img/jp.svg
-     3742  2022-01-04 00:49   img/zh.svg
-   221580  2022-01-04 00:45   img/spa.svg
-     1995  2022-01-04 00:48   img/de.svg
-     1822  2022-01-04 00:48   img/en.svg
-     1403  2022-01-04 00:43   img/ru.svg
-     2600  2022-01-04 00:43   img/kor.svg
-    14485  2022-01-04 15:58   img/translator-symbolic.svg
-     4462  2022-01-04 00:49   img/ara.svg
-     1296  2022-01-04 00:44   img/fra.svg
-    14263  2022-01-04 16:03   img/trans-symbolic.svg
-        0  2022-01-07 12:01   locale/
-        0  2022-01-07 12:01   locale/zh_CN/
-        0  2022-01-07 12:02   locale/zh_CN/LC_MESSAGES/
-      535  2022-01-07 12:02   locale/zh_CN/LC_MESSAGES/clip-translator.mo
----------                     -------
-   299555                     21 files
-
-⭕ zip -u -v clip-translator@eexpss.gmail.com.zip
+# 自动编译 po/zh_CN.po 和 schemas/*.xml 文件。
+⭕ gnome-extensions pack -f --extra-source=clip-note-symbolic.svg
 
 ## 排错
 ⭕ ag clip-note /var/log/syslog|grep 'Jan 25'|egrep -v '===clip-note===|JS ERROR|inflating|extracting|ignoring null'
